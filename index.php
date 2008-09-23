7e3e993bb46a97c2e99ad4b8cad271c6c614896d
H. Peter Anvin <hpa@zytor.com>
2008-09-22 22:44:33 -0700

    This is 2008, use UTF-8

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
-  <meta content="text/html; charset=ISO-8859-1" http-equiv="content-type" />
+  <meta content="text/html; charset=UTF-8" http-equiv="content-type" />
   <link rel="stylesheet" type="text/css" href="css/base.css" />
   <title>The Netwide Assembler: NASM</title>
 </head>
