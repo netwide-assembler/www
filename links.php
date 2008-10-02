1f18ee4dc4aacc6c7342288e0483e32ea79a1c40
H. Peter Anvin <hpa@zytor.com>
2008-10-01 18:47:02 -0700

    links.php: remove Sourceforge logo
    We're no longer hosting the web pages on Sourceforge; remove logo.

--- links.php
+++ links.php
@@ -66,9 +66,6 @@
 </div>
 
 <p style="text-align: right;"><br />
- <a href="http://sourceforge.net">
-  <img src="http://sourceforge.net/sflogo.php?group_id=6208&amp;type=1" width="88" height="31" alt="SourceForge.net Logo" style="border: none;" />
- </a>
  <a href="http://validator.w3.org/check?uri=referer">
   <img src="http://www.w3.org/Icons/valid-xhtml10" alt="Valid XHTML 1.0 Strict" height="31" width="88" style="border: none;" />
  </a>
