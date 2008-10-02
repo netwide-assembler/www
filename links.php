9a046c2216f494ea19aa95b6b48312ef6c472d30
H. Peter Anvin <hpa@zytor.com>
2008-10-01 18:58:57 -0700

    Consistent prologue and character set declarations

--- links.php
+++ links.php
@@ -1,10 +1,11 @@
 <?= '<?xml version="1.0" encoding="utf-8"?>' ?>
+
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
    "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
 <head>
 
-  <meta content="text/html; charset=ISO-8859-1" http-equiv="content-type" />
+  <meta content="text/html; charset=UTF-8" http-equiv="content-type" />
   <link rel="stylesheet" type="text/css" href="css/base.css" />
   <title>The Netwide Assembler: NASM</title>
 
