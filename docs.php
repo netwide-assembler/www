6d9687c4d89cd9eb2069676ad53cd17f85ee4402
H. Peter Anvin <hpa@zytor.com>
2010-11-08 09:01:32 -0800

    docs.php: point to nasmdoc0.html, avoid double slash

--- docs.php
+++ docs.php
@@ -42,12 +42,12 @@
   </div>
 
   <?php
-    $onl_path = "http://www.nasm.us/xdoc/";
+    $onl_path = "http://www.nasm.us/xdoc";
     if ($version) {
         echo "<div class=\"item\">\n";
         echo "<h1>; Online Documenation for NASM-$version (stable version)</h1>\n";
         echo "<ul>\n";
-        echo "<li><a href=\"$onl_path/$version/html/\">HTML format</a></li>\n";
+        echo "<li><a href=\"$onl_path/$version/html/nasmdoc0.html\">HTML format</a></li>\n";
         echo "<li><a href=\"$onl_path/$version/nasmdoc.pdf\">PDF format</a></li>\n";
         echo "</ul>\n";
         echo "</div>\n";
@@ -56,7 +56,7 @@
         echo "<div class=\"item\">\n";
         echo "<h1>; Online Documenation for NASM-$version (release candidate)</h1>\n";
         echo "<ul>\n";
-        echo "<li><a href=\"$onl_path/$rc_version/html/\">HTML format</a></li>\n";
+        echo "<li><a href=\"$onl_path/$rc_version/html/nasmdoc0.html\">HTML format</a></li>\n";
         echo "<li><a href=\"$onl_path/$rc_version/nasmdoc.pdf\">PDF format</a></li>\n";
         echo "</ul>\n";
         echo "</div>\n";
