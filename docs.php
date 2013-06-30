f2c0dc7d81dfd13c36d6f24a59fa23b03303e3f8
Cyrill Gorcunov <gorcunov@gmail.com>
2013-06-30 14:03:53 +0400

    docs: Shrink text
    Signed-off-by: Cyrill Gorcunov <gorcunov@gmail.com>

--- docs.php
+++ docs.php
@@ -13,21 +13,21 @@
 
         <h1>NASM Documenation</h1>
         <p>The NASM documentation is auto-generated on every new release (including
-        release candidates) and accessible either online or in various offline
+        release candidates, that named -rc) and accessible either online or in various offline
         archives. Such archives provide documentation in the following formats:
         PDF, PostScript, plain text, HTML, and info.</p>
 
         <?php
             $onl_path = "http://www.nasm.us/xdoc";
             if ($version) {
-                echo "<h1>Online Documenation for NASM $version (stable version)</h1>\n";
+                echo "<h1>Online for NASM $version (stable)</h1>\n";
                 echo "<ul>\n";
                 echo "<li><a href=\"$onl_path/$version/html/nasmdoc0.html\">HTML format</a></li>\n";
                 echo "<li><a href=\"$onl_path/$version/nasmdoc.pdf\">PDF format</a></li>\n";
                 echo "</ul>\n";
             }
             if ($rc_version) {
-                echo "<h1>Online Documenation for NASM $rc_version (release candidate)</h1>\n";
+                echo "<h1>Online for NASM $rc_version (-rc)</h1>\n";
                 echo "<ul>\n";
                 echo "<li><a href=\"$onl_path/$rc_version/html/nasmdoc0.html\">HTML format</a></li>\n";
                 echo "<li><a href=\"$onl_path/$rc_version/nasmdoc.pdf\">PDF format</a></li>\n";
@@ -39,7 +39,7 @@
             $pkg_path = "http://www.nasm.us/pub/nasm/releasebuilds";
             if ($version) {
                 $pkg_name = "nasm-$version-xdoc";
-                echo "<h1>Download Documenation for NASM $version (stable version)</h1>\n";
+                echo "<h1>Downloadable for NASM $version (stable)</h1>\n";
                 echo "<ul>\n";
                 echo "<li><a href=\"$pkg_path/$version/$pkg_name.tar.bz2\">$pkg_name.tar.bz2</a></li>\n";
                 echo "<li><a href=\"$pkg_path/$version/$pkg_name.tar.gz\">$pkg_name.tar.gz</a></li>\n";
@@ -48,7 +48,7 @@
             }
             if ($rc_version) {
                 $pkg_name = "nasm-$rc_version-xdoc";
-                echo "<h1>Download Documenation for NASM $rc_version (release candidate)</h1>\n";
+                echo "<h1>Downloadable for NASM $rc_version (-rc)</h1>\n";
                 echo "<ul>\n";
                 echo "<li><a href=\"$pkg_path/$rc_version/$pkg_name.tar.bz2\">$pkg_name.tar.bz2</a></li>\n";
                 echo "<li><a href=\"$pkg_path/$rc_version/$pkg_name.tar.gz\">$pkg_name.tar.gz</a></li>\n";
