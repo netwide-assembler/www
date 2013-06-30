02bb1d79934454eb95636fd05356251516554ae6
Cyrill Gorcunov <gorcunov@gmail.com>
2013-06-30 13:56:37 +0400

    Move common header into header.inc
    Signed-off-by: Cyrill Gorcunov <gorcunov@gmail.com>

--- patches.php
+++ patches.php
@@ -2,12 +2,7 @@
 
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-  <meta content="text/html; charset=UTF-8" http-equiv="content-type" />
-  <link rel="stylesheet" type="text/css" href="css/base.css" />
-  <link rel="icon" type="image/vnd.microsoft.icon" href="/favicon.ico" />
-  <title>The Netwide Assembler: NASM</title>  
-</head>
+<?php include "header.inc" ?>
 
 <body id="wrap">
 
