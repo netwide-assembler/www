53ab1f2024907dfc8be129f5b6ad211d38938361
Cyrill Gorcunov <gorcunov@gmail.com>
2014-07-09 23:24:20 +0400

    move it into proper place
    Signed-off-by: Cyrill Gorcunov <gorcunov@gmail.com>

--- index.php
+++ index.php
@@ -2,7 +2,6 @@
 
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
-<meta name="globalsign-domain-verification" content="rnHLlAfdI6izlAhGis3b5oEthJxvokH5i9F5hZgUpO" />
 
 <?php include "header.inc" ?>
 
