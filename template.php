3b2e8dae75be128099919ec372ea498dc6539e03
Cyrill Gorcunov <gorcunov@gmail.com>
2015-03-22 16:40:17 +0300

    Add template.php
    For future use if needed.
    
    Signed-off-by: Cyrill Gorcunov <gorcunov@gmail.com>

---
+++ template.php
@@ -0,0 +1,15 @@
+<?php echo '<?xml version="1.0" encoding="utf-8"?>'; ?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<?php include "header.inc" ?>
+
+<body>
+	<?php include "navbar.inc" ?>
+
+	<!-- Put new code here -->
+
+	<?php include "footer.inc" ?>
+</body>
+</html>
